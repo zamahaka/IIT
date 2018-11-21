7ddfe122da2b9f6d1c148ce85d75fd8f37b75b72
Taras <tarassmakula@gmail.com>
2018-11-21 18:51:56 +0200

    Move lab3 to taras(25_26)

---
+++ src/main/kotlin/taras/lab3/Lab3_25.kt
@@ -0,0 +1,13 @@
+package taras.lab3
+
+
+fun main() {
+    val results = Elector(candidates, cities).elect()
+
+    println("Results:")
+    results.printResults()
+    println()
+    
+    println("Looser is: ${results.findMinCandidate().name}")
+}
+
